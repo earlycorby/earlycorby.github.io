681fe81622b61c3529144df8e5ff2b85444b27b9
Anne Corby <earlycorby@gmail.com>
2019-08-05 15:30:46 +1000

    testing xml loading

---
+++ challenge/weather.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+function loadXMLDoc(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+    xmlhttp.open("GET", "C:/Users/Anne/Downloads/IDV10752.xml", true);
+    xmlhttp.send();
+}
+
+function myFunction(xml){
+    var xmlDoc = xml.responseXML;
+    var x = document.getElementsByTagName("aac");
+    document.getElementById("VIC_PT043").innerHTML = x[0].childNodes[0].nodeValue;
+}
