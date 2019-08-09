2385ca9b4c5c177e5f7feaa9f26f220de8d1696e
Anne Corby <earlycorby@gmail.com>
2019-08-09 14:26:26 +1000

    updated to include challenge 4

--- js/findWeather.js
+++ js/findWeather.js
@@ -27,3 +27,20 @@ function getWeather() {
     xhttp.send();
 
 }
+
+function whatClothing(temperature) {
+    if (temperature < 20) {
+        clothing = "longs and longs";
+        document.getElementById("clothes").innerHTML = clothing;
+
+    }
+    else if (20 <= temperature < 25) {
+        clothing = "shorts and longs";
+        document.getElementById("clothes").innerHTML = clothing;
+    }
+    else if (temperature >= 25) {
+        clothing = "shorts and shorts";
+        document.getElementById("clothes").innerHTML = clothing;
+    }
+
+}
--- js/whatClothing.js
+++ js/whatClothing.js
@@ -2,13 +2,16 @@
 function whatClothing(temperature) {
     if (temperature < 20) {
         clothing = "longs and longs";
+        document.getElementById("clothes").innerHTML = clothing;
 
     }
     else if (20 <= temperature < 25) {
         clothing = "shorts and longs";
+        document.getElementById("clothes").innerHTML = clothing;
     }
     else if (temperature >= 25) {
         clothing = "shorts and shorts";
+        document.getElementById("clothes").innerHTML = clothing;
     }
-    document.getElementById("clothes").innerHTML = clothing;
+   
 }
