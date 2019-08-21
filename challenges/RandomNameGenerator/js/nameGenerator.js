d7aa86db1a90786efe8851b66110ee032ca035cd
Anne Corby <earlycorby@gmail.com>
2019-08-21 13:06:35 +1000

    testing files

--- challenges/RandomNameGenerator/js/nameGenerator.js
+++ challenges/RandomNameGenerator/js/nameGenerator.js
@@ -1,1 +1,36 @@
 // JavaScript source code
+function getFemale() {
+   document.getElementById("result").innerHTML = getName("FEMALE");
+}
+function getMale() {
+    document.getElementById("result").innerHTML = getName("MALE");
+}
+function getAny() {
+    document.getElementById("result").innerHTML = getName("ANY");
+}
+
+function getName(gender) {
+
+    var fileOpen = new XMLHttpRequest();
+    fileOpen.open('GET', 'babynamesfromAll.json');
+    fileOpen.onreadystatechange = function () {
+        alert(fileOpen.responseText);
+    }
+    fileOpen.send();
+    
+    
+
+    if (gender === "FEMALE") {
+
+    } else if (gender === "MALE") {
+
+    }
+    else {
+
+    }
+
+    return myObj[0][0];
+
+
+
+}
