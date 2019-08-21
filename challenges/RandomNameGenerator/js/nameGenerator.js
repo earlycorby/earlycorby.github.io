8f4630a54bf656b5a59399c8b8260ab8c971fe43
Anne Corby <earlycorby@gmail.com>
2019-08-21 13:21:41 +1000

    Update nameGenerator.js

--- challenges/RandomNameGenerator/js/nameGenerator.js
+++ challenges/RandomNameGenerator/js/nameGenerator.js
@@ -10,12 +10,12 @@ function getAny() {
 }
 
 function getName(gender) {
-    var myObj = new JSON;
+    
     var fileOpen = new XMLHttpRequest();
     fileOpen.open('GET', './babynamesfromAll.json');
     fileOpen.onreadystatechange = function () {
         // alert(fileOpen.responseText);
-        myObj = this.JSON;
+        myObj = JSON.parse(this);
     }
     fileOpen.send();
     
