dc8a1767a31c447570b7e582925263266930c0d7
Anne Corby <earlycorby@gmail.com>
2019-08-22 11:02:09 +1000

    changing how the data loads

---
+++ challenges/RandomNameGenerator/js/loadNames.js
@@ -0,0 +1,29 @@
+var girls, boys, all;
+
+loadfile(){
+   
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            for (var i = 0; i < myObj.length; i++) {
+                var nextGender = myObj[i].gender;
+                var nextName = myObj[i].name;
+                if (nextGender === "FEMALE") {
+                    console.log("Girls");
+                    girls.push(nextName);
+                    all.push(nextName);
+                } else if (nextGender === "MALE") {
+                    console.log("boys");
+                    boys.push(nextName);
+                    all.push(nextName);
+                } 
+            }  
+        }
+    };
+    xhttp.open("GET", "./babynamesfromAll.json", true);
+    xhttp.send();
+
+
+    return [girls, boys, all];
+}
